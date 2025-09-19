35108698cbeab8d36f462a6fbdc566b25958f6e1
nodirchik6903 <nu2913634@gmail.com>
2025-09-19 16:46:22 +0500

    edits admins

--- config/createAdminDefault.js
+++ config/createAdminDefault.js
@@ -1,20 +1,23 @@
+require("dotenv").config();
 const Admin = require("../models/Admin")
 const bcrypt = require("bcryptjs")
+
+const admin_username = process.env.ADMIN_USERNAME
+const admin_password  = process.env.ADMIN_PASSWORD
 
 
 const createAdminDefault = async () => {
     try {
-        const adminExists = await Admin.findOne({username: "Nodirbek"})
+        const adminExists = await Admin.findOne({username: admin_username})
 
         if(!adminExists){
-            const hashedPassword = await bcrypt.hash("Nodirbek6903",10)
+            const hashedPassword = await bcrypt.hash(admin_password,10)
 
             await Admin.create({
-                username: "Nodirbek",
+                username: admin_username,
                 password: hashedPassword
             })
-
-            console.log("Default admin yaratildi: Nodirbek / Nodirbek6903");
+            
         }else{
             console.log("Admin allaqachon mavjud");
         }
